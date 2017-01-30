5e2a711c31df8703485fece0dbf9d781023f4b6e
Michael J. Simons <michael@simons.ac>
2017-01-30 21:48:47 +0100

    Add forgotton run call

--- src/main/java/de/springbootbuch/helloworld_war/Application.java
+++ src/main/java/de/springbootbuch/helloworld_war/Application.java
@@ -38,7 +38,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        configureApplication(new SpringApplicationBuilder((Object[])args));
+        configureApplication(new SpringApplicationBuilder((Object[])args)).run(args);
     }
     
     private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
