899bc582a8710645561e502f5a6c0d74afdb870b
Michael J. Simons <michael@simons.ac>
2017-01-31 08:50:57 +0100

    Remove Spring-Boot-Maven-Plugin
    The executable WAR is nice from a technical point of view, but I think that people who want to deploy as war want their war as small as possibile.

--- src/main/java/de/springbootbuch/helloworld_war/Application.java
+++ src/main/java/de/springbootbuch/helloworld_war/Application.java
@@ -2,7 +2,6 @@ package de.springbootbuch.helloworld_war;
 
 import java.util.List;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,18 +29,5 @@ public class Application extends SpringBootServletInitializer {
         public List<GreetingEntity> greetings() {
             return this.greetingRepository.findAll(new Sort("value"));
         }
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return configureApplication(builder);
-    }
-
-    public static void main(String[] args) {
-        configureApplication(new SpringApplicationBuilder()).run(args);
-    }
-    
-    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 }
